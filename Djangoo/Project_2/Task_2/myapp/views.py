07001d02c5d441de4192f8b8d4c643580bb4b708
Vishalrogers28 <vishalingale135@gmail.com>
2024-09-20 11:22:24 +0530

    Added new files

--- Djangoo/Project_2/Task_2/myapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from myapp.models import MyModel
-import threading
-
-print(f"Caller Thread ID: {threading.get_ident()}")
-MyModel.objects.create(name="Test Instance")
-
